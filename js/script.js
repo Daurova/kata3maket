35e9a813fc47fbd2fb8d53ed3a6e282049797c62
Daurova <147259989+Daurova@users.noreply.github.com>
2023-12-30 11:39:17 +0300

    kata3maket

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+let buttonShow = document.querySelector('.button-show-hide');
+let brands = document.querySelector('.brands')
+let brandNames = document.querySelector('.brand-names')
+let arrow = document.querySelector('.show-hide-arrow')
+
+
+let showAll = function(){
+brands.classList.toggle('full');
+brandNames.classList.toggle('full');
+buttonShow.classList.toggle('show-hide')  
+buttonShow.classList.toggle('button-show-hide')
+arrow.classList.toggle('show-arrow')
+arrow.classList.toggle('hide-arrow')
+}
+
+buttonShow.addEventListener('click', showAll);

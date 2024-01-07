70cd5bfee03e1a1ce376769d3b8c1f3aa827bd41
Daurova <147259989+Daurova@users.noreply.github.com>
2024-01-07 12:25:23 +0300

    jhb

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 
 let buttonShow = document.querySelector('.button-show-hide');
-let brands = document.querySelector('.brands')
-let brandNames = document.querySelector('.brand-names')
+let brands = document.querySelector('.swiper')
+let brandNames = document.querySelector('.swiper-wrapper')
 let arrow = document.querySelector('.show-hide-arrow')
 
 
@@ -15,3 +15,37 @@ arrow.classList.toggle('show-arrow')
 arrow.classList.toggle('hide-arrow')
 }
 buttonShow.addEventListener('click', showAll);
+
+
+let swiper;
+
+function handleSwiper() {
+  if (window.matchMedia('(max-width: 768px)').matches) {
+
+    if (!swiper) {
+      swiper = new Swiper('.swiper', {
+        loop: false,
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+    }
+  } else {
+    if (swiper) {
+      swiper.destroy();
+      swiper = undefined;
+    }
+  }
+}
+window.addEventListener('resize', handleSwiper);
+
+handleSwiper();
+
+
